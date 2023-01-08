9b1304625f949294edbd3c284740f34ecefc1723
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-01-08 11:46:42 +0100

    Add simple subtract, multiply, divide

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ const add = function(a, b) {
     return a+b;
 };
 
+const subtract = function (a, b) {
+    return a-b;
+};
+
+const multiply = function(a, b) {
+    return a*b;
+};
+
+const divide = function(a, b) {
+    return a/b;
+};
